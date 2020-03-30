92bd93173a721d005d08fba6c68b372a44fa0d90
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2020-03-30 23:55:39 +0200

    move secret key to env

--- src/config/environments/production.rb
+++ src/config/environments/production.rb
@@ -10,6 +10,7 @@ Rails.application.configure do
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
+  config.require_master_key = false
 
   if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
