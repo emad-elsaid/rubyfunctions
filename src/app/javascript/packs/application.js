bac51e8a0163dc303d08409b2132354eb1f8c274
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2020-03-31 19:10:29 +0200

    navbar hamburger menu toggle for mobile view

--- src/app/javascript/packs/application.js
+++ src/app/javascript/packs/application.js
@@ -5,6 +5,7 @@
 
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
+require("navbar")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
