52eb0dcfd6746da00939b9b64b3fdfb9e29fd9da
Ahmed Khaled <ahmed.khaled1512@yahoo.com>
2020-04-02 19:25:22 +0200

    get the function name from its code

--- src/app/models/function.rb
+++ src/app/models/function.rb
@@ -1,10 +1,12 @@
 class Function < ApplicationRecord
-  validates :name, presence: true, uniqueness: { scope: :user_id }, format: { with: /\A[0-9a-z_]+\z/i }
+  validates :name, presence: true, uniqueness: { scope: :user_id }
   validates :usage, presence: true
   validates :code, presence: true
   validates :user, presence: true
 
   belongs_to :user
+
+  before_validation :function_name
 
   def to_param
     name
@@ -12,5 +14,25 @@ class Function < ApplicationRecord
 
   def self.from_param(param)
     find_by(name: param)
+  end
+
+  private
+
+  def find_first_function(ast)
+    return ast.children.first.to_s if ast.respond_to?('type') && ast.type == :DEFN
+
+    return unless ast.respond_to?('children')
+
+    ast.children.each do |child|
+      func_name = find_first_function(child)
+      return func_name if func_name
+    end
+  end
+
+  def function_name
+    self.name = find_first_function(RubyVM::AbstractSyntaxTree.parse(code))
+  rescue SyntaxError
+    errors.add(:has_syntax_error, 'code has syntax error')
+    throw(:abort)
   end
 end
