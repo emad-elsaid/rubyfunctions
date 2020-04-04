b6a668e77a9c8bda8837482370f2d8063fa3e362
Ahmed Magdy <ahmadmgdi94@gmail.com>
2020-04-04 18:59:30 +0200

    Add Required Shell Scripts for Entrypoint and Wait For It, that Waits For a Service to be Fully Up and Running

---
+++ src/scripts/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+./scripts/wait-for-it.sh -t 0 $POSTGRES_HOST:$POSTGRES_PORT
+
+kill -9 $(cat /app/tmp/pids/server.pid)
+rm -rf /app/tmp/pids/server.pid
+yarn install --check-files
+
+bundle config set with 'development'
+bundle install
+
+bundle exec rails db:exists || bundle exec rails db:setup
+
+bundle exec rails s -b 0.0.0.0 -p 3000
