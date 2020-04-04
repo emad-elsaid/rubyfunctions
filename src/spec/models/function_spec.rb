760307a16a93b06fe02adeec20795b2234e70b47
Ahmed Khaled <ahmed.khaled1512@yahoo.com>
2020-04-05 00:41:33 +0200

    Remove shoulda matchers

--- src/spec/models/function_spec.rb
+++ src/spec/models/function_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 
+
 RSpec.describe Function, type: :model do
   subject { create :function }
 
+  def generate_name (code)
+    subject.code = code
+    subject.validate
+  end
+
   describe '#name' do
     it 'must exist' do
-      subject.code = 'def;end'
-      subject.validate
+      generate_name 'def;end'
       expect(subject.errors[:name]).to be_present
     end
+
 
     it 'must be unique' do
       duplicate = subject.dup
@@ -17,13 +23,30 @@ RSpec.describe Function, type: :model do
     end
   end
 
-  # it { is_expected.to validate_uniqueness_of(:name).scoped_to(:user_id) }
-  # it { is_expected.to allow_value('function_name').for :name }
-  # it { is_expected.to allow_value('functionName').for :name }
-  # it { is_expected.to allow_value('function1').for :name }
-  # it { is_expected.to_not allow_value('name with space').for :name }
-  # it { is_expected.to_not allow_value('name_with\backslash').for :name }
-  # it { is_expected.to_not allow_value('اسم_عربي').for :name }
+  it 'allow camel case' do
+    generate_name 'def cameCase; end'
+    expect(subject).to be_valid
+  end
+
+  it 'allow to have number in it' do 
+    generate_name 'def foo1; end'
+    expect(subject).to be_valid
+  end
+
+  it 'don\'t allow space' do
+    generate_name('def fo o a; x = 1; end')
+    expect(subject).to_not be_valid
+  end
+
+  it 'don\'t allow backlash' do
+    generate_name 'def fo\o; x = 1; end'
+    expect(subject).to_not be_valid
+  end
+
+  it 'don\'t allow اسم عربي' do
+    generate_name 'def عربي; x = 1; end'
+    expect(subject).to_not be_valid
+  end
 
   it { is_expected.to validate_presence_of :usage }
   it { is_expected.to validate_presence_of :code }
