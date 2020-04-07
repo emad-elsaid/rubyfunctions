c747ac996784829a0beb8399c831257787b8c1e8
Ahmed Magdy <ahmadmgdi94@gmail.com>
2020-04-07 02:18:57 +0200

    Make Github Hyperlink for Username

--- src/app/helpers/users_helper.rb
+++ src/app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
   def user_image_url(user)
     "https://avatars0.githubusercontent.com/u/#{user.github_id}?v=4"
   end
+
+  def user_github_account_url(user)
+    "https://github.com/#{user.username}"
+  end
 end
