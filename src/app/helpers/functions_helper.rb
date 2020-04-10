778526d3978f98535a3e96b27441bdab70682ead
Ahmed Khaled <ahmed.khaled1512@yahoo.com>
2020-04-10 22:03:39 +0200

    Convert function usage and comments markdown to html

--- src/app/helpers/functions_helper.rb
+++ src/app/helpers/functions_helper.rb
@@ -7,4 +7,13 @@ module FunctionsHelper
     sanitized = sanitize(formatter.format(lexer.lex(ruby)), tags: %w[div span], attributes: %w[style])
     tag.pre sanitized
   end
+
+  def render_markdown(str)
+    options = { input: 'GFM', auto_id_stripping: true, syntax_highlighter: nil }
+    allowed_tags = %w[p a ul ol li del strong em blockquote code pre span div]
+    allowed_attributes = %w[href rel]
+
+    kramdown_doc = Kramdown::Document.new(str, options)
+    sanitize(kramdown_doc.to_html_from_markdown, tags: allowed_tags, attributes: allowed_attributes)
+  end
 end
