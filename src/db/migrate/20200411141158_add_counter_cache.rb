205db865ff77c91b03c6eb6c4466376175b40fec
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2020-04-11 18:54:47 +0200

    fix the migration

--- src/db/migrate/20200411141158_add_counter_cache.rb
+++ src/db/migrate/20200411141158_add_counter_cache.rb
@@ -6,18 +6,18 @@ class AddCounterCache < ActiveRecord::Migration[6.0]
 
   def change_users
     change_table :users, bulk: true do |t|
-      t.add_column :comments_count, :integer
-      t.add_column :functions_count, :integer
-      t.add_column :likes_count, :integer
-      t.add_column :saves_count, :integer
+      t.column :comments_count, :integer
+      t.column :functions_count, :integer
+      t.column :likes_count, :integer
+      t.column :saves_count, :integer
     end
   end
 
   def change_functions
     change_table :functions, bulk: true do |t|
-      t.add_column :comments_count, :integer
-      t.add_column :likes_count, :integer
-      t.add_column :saves_count, :integer
+      t.column :comments_count, :integer
+      t.column :likes_count, :integer
+      t.column :saves_count, :integer
     end
   end
 end
