e9f2b5e332bd56046bcfd3c00f9715bc311afdad
Ahmed Magdy <ahmadmgdi94@gmail.com>
2020-04-18 16:03:34 +0200

    Refactor Follower List, Use User Card

--- src/app/helpers/users_helper.rb
+++ src/app/helpers/users_helper.rb
@@ -6,4 +6,9 @@ module UsersHelper
   def user_github_url(user)
     "https://github.com/#{user.username}"
   end
+
+  def show_actions?
+    page_title = content_for(:title).to_param
+    page_title && !page_title.include?("Followers")
+  end
 end
