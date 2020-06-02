6298222eeb1b973007698a772dc3b82f3e658bd7
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2020-06-02 13:03:27 +0200

    Handle record not found exception in production

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
-  include SentryConcern if Rails.env.production?
+  include SentryConcern if Rails.application.config_for(:rescue)[:report_to_sentry]
   include PermissionsConcern
 
   rescue_from UnauthorizedException, with: :unauthorized
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found if Rails.application.config_for(:rescue)[:record_not_found]
 
   helper_method :current_user
 
@@ -16,5 +17,9 @@ class ApplicationController < ActionController::Base
 
   def unauthorized
     redirect_to root_path, alert: 'You do not have enough permissions to perform this action'
+  end
+
+  def not_found
+    redirect_to '/404.html'
   end
 end
--- src/config/initializers/sentry.rb
+++ src/config/initializers/sentry.rb
@@ -1,1 +1,1 @@
-Raven.configure { |config| config.dsn = ENV['SENTRY_DSN'] } if Rails.env.production?
+Raven.configure { |config| config.dsn = ENV['SENTRY_DSN'] } if Rails.application.config_for(:rescue)[:report_to_sentry]
