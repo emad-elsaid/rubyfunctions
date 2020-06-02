0404a7a30406ca5e5a02a855f01a216dfabf2b5a
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2020-06-02 14:45:45 +0200

    improve tests

--- src/spec/controllers/tags_controller_spec.rb
+++ src/spec/controllers/tags_controller_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe TagsController do
+  render_views
+
   describe 'GET #show' do
-    let!(:functions_count) { 3 }
-
     let(:tag) { create :tag }
-    let(:functions) { create_list :function, functions_count, tags: [tag] }
+    let!(:functions) { create_list :function, 3, tags: [tag] }
+    let(:functions_names) { functions.map(&:name) }
 
-    it 'returns all the funtions associated with this tag' do
-      get :show, params: { id: tag }
+    before { get :show, params: { id: tag } }
+
+    it 'returns all the functions associated with this tag' do
       expect(response).to be_successful
+    end
+
+    it 'returns all functions name in response' do
+      expect(response.body).to include(*functions_names)
     end
   end
 end
