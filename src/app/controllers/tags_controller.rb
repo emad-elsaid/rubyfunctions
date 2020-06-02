61c613e0256a045d37241408337ae8976915b8b9
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2020-06-02 13:12:42 +0200

    isolated getting the tag to a private function

--- src/app/controllers/tags_controller.rb
+++ src/app/controllers/tags_controller.rb
@@ -1,15 +1,20 @@
 class TagsController < ApplicationController
   LIMIT = 30
 
+  before_action :set_tag
+
   def show
     offset = params.fetch(:offset, 0).to_i
 
-    @tag = params[:id]
-    @functions = Tag.from_param(@tag).functions.limit(LIMIT).offset(offset).order(likes_count: :desc).all ||
-                 raise(ActiveRecord::RecordNotFound)
-
+    @functions = @tag.functions.limit(LIMIT).offset(offset).order(likes_count: :desc).all
     @next = offset + LIMIT if @functions.size == LIMIT
   end
 
   def index; end
+
+  private
+
+  def set_tag
+    @tag = Tag.from_param(params[:id]) || raise(ActiveRecord::RecordNotFound)
+  end
 end
